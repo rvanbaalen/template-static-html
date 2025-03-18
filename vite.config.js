c73c774292aff482845f9c3f65415f88335a9987
Robin van Baalen <robin@ganttify.com>
2025-03-17 20:33:35 -0400

    feat: initialize project structure with essential files and configurations

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  //base: "/custom-base-url/", // Change this if your page is not hosted at a root URL
+  plugins: [
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // otherName: resolve(__dirname, 'src/other-file.html'), // If you're publishing multiple pages
+      },
+    },
+  },
+})
